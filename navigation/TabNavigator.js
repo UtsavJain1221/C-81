6a8edd8864936b56f0947028cab5ddc278d7f5f2
UtsavJain1221 <62596506+UtsavJain1221@users.noreply.github.com>
2023-06-30 12:55:13 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,5 @@
+import react from "react";
+import {CreateBottomTabNavigator} from "@react-navigation/bottom-Tabs";
+import {Ionicons} from "react-native-vector-Icons/Ionicons";
+import {Feed} from "../screens/Feed";
+import {CreateStory} from "../screens/CreateStory";
